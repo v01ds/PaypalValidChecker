546c0fbc25534b38f1ec73e9d9f956428761f72f
v01ds <59950214+v01ds@users.noreply.github.com>
2020-12-15 13:51:51 +0300

    Update realchecker.py

--- PythonValidChecker/realchecker.py
+++ PythonValidChecker/realchecker.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.chrome.options import Options
+import time
 
 
 
@@ -27,12 +28,11 @@ list = open(list_inp, 'r')
 print("-"*55)
 
 
-while True:
-
 # Opening the browser
-    
-    driver = webdriver.Chrome('C:\Users\90538\Documents\ChromeDriver\chromedriver.exe') #Write your own path here...
-    
+driver = webdriver.Chrome('C:\Users\90538\Documents\ChromeDriver\chromedriver.exe') #Write your own path here...
+
+while True:
+   
     
 # Choose mails from the list
     email = list.readline().replace('\n','')
@@ -45,33 +45,28 @@ while True:
 # Go to Paypal's website and enter the mail address    
     driver.get("https://www.paypal.com/bizsignup/#/checkAccount")
     
-    timeout_email = 4 #Seconds
+    timeout_email = 5 #Seconds
     mail_present = EC.presence_of_element_located((By.ID, 'email'))
     WebDriverWait(driver, timeout_email).until(mail_present)
     
     found_mail = driver.find_element_by_id("email")
     found_mail.send_keys(real_mail)
     driver.find_element_by_id("continueButton").click()
-
-# Getting the redirected link and checking if the account is valid or not.
-    live_status = False
-    try:
-        timeout_pass = 5 #Seconds
-        pass_present = EC.presence_of_element_located((By.ID, 'password'))
-        WebDriverWait(driver, timeout_pass).until(pass_present)
     
-    except:
-        print("\033 Live | "+email+" | [(Checked)]")
-        live.write(email + '\n')
-        live_status = True
-            
-    if not live_status:
-        print("\033 Die | "+email+" | [(Checked)]")
-        die.write(email + '\n')
+    time.sleep(1)
+    
+# Getting the redirected link and checking if the account is valid or not.
+    current = driver.current_url
+    
+    if current == 'https://www.paypal.com/bizsignup/#/checkAccount':
+        print("\033 Live | "+real_mail+" | [(Checked)]")
+        live.write(real_mail + '\n')
+    else:
+        print("\033 Die | "+real_mail+" | [(Checked)]")
+        die.write(real_mail + '\n')
         
-    driver.close()
-        
-        
+    
+    driver.refresh()        
     
 # More show... (and quit)
 print("-"*50)
@@ -81,5 +76,4 @@ driver.quit()
     
 
     
-
 
